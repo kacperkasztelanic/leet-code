e840831e0d4337717ac8713023bdcbbb66a64a47
Kacper Kasztelanic <kcpr51@hotmail.com>
2019-04-06 19:44:46 +0200

    Add MergeTwoSortedLists

---
+++ src/solutions/scala/N21.scala
@@ -0,0 +1,53 @@
+package solutions.scala
+
+import scala.annotation.tailrec
+
+object N21 extends App {
+
+  def mergeTwoLists(l1: List[Int], l2: List[Int]): List[Int] = {
+    @tailrec
+    def mergeTwoLists(ll1: List[Int], ll2: List[Int], acc: List[Int]): List[Int] = {
+      (ll1, ll2) match {
+        case (Nil, Nil) => acc
+        case (a, Nil) => acc ::: a
+        case (Nil, b) => acc ::: b
+        case (h1 :: t1, h2 :: t2) =>
+          if (h1 < h2) mergeTwoLists(t1, h2 :: t2, acc ::: List(h1))
+          else mergeTwoLists(h1 :: t1, t2, acc ::: List(h2))
+      }
+    }
+
+    mergeTwoLists(l1, l2, Nil)
+  }
+
+  println(mergeTwoLists(List(1, 5, 10), List(4, 5, 11, 12)))
+
+  case class ListNode(var x: Int = 0, var next: ListNode = null)
+
+  def mergeTwoLists(l1: ListNode, l2: ListNode): ListNode = {
+    @tailrec
+    def append(ll1: ListNode, ll2: ListNode, head: ListNode): ListNode = {
+      if (ll1 == null) ll2
+      else if (ll1.next == null) {
+        ll1.next = ll2
+        head
+      } else append(ll1.next, ll2, head)
+    }
+
+    @tailrec
+    def mergeTwoLists(ll1: ListNode, ll2: ListNode, acc: ListNode): ListNode = {
+      if (ll1 == null && ll2 == null) acc
+      else if (ll1 != null && ll2 == null) append(acc, ll1, acc)
+      else if (ll1 == null && ll2 != null) append(acc, ll2, acc)
+      else {
+        if (ll1.x < ll2.x) mergeTwoLists(ll1.next, ll2, append(acc, ListNode(ll1.x), acc))
+        else mergeTwoLists(ll1, ll2.next, append(acc, ListNode(ll2.x), acc))
+      }
+    }
+
+    mergeTwoLists(l1, l2, null)
+  }
+
+
+  println(mergeTwoLists(ListNode(1, ListNode(5, ListNode(10))), ListNode(4, ListNode(5, ListNode(11, ListNode(12))))))
+}
