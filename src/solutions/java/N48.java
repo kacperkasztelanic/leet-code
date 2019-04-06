c269edd283a3c7cdc0b81f65053d2c66bf49d5ac
Kacper Kasztelanic <kcpr51@hotmail.com>
2019-04-06 20:57:24 +0200

    Add rotate2dMatrix

---
+++ src/solutions/java/N48.java
@@ -0,0 +1,42 @@
+package solutions.java;
+
+import java.util.Arrays;
+
+public class N48 {
+
+    public static void rotate(int[][] matrix) {
+        transpose(matrix);
+        reverseRows(matrix);
+    }
+
+    private static void transpose(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i; j < matrix[i].length; j++) {
+                int temp = matrix[j][i];
+                matrix[j][i] = matrix[i][j];
+                matrix[i][j] = temp;
+            }
+        }
+    }
+
+    private static void reverseRows(int[][] matrix) {
+        for (int[] row : matrix) {
+            reverseRow(row);
+        }
+    }
+
+    private static void reverseRow(int[] row) {
+        for (int i = 0; i < row.length / 2; i++) {
+            int temp = row[row.length - 1 - i];
+            row[row.length - 1 - i] = row[i];
+            row[i] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        System.out.println(Arrays.deepToString(matrix));
+        N48.rotate(matrix);
+        System.out.println(Arrays.deepToString(matrix));
+    }
+}
